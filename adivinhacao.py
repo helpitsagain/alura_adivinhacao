973ca4d80a2a4e89515cb13fcb73b8980c4e2ccd
Luc <lucthemit@gmail.com>
2022-06-23 16:48:29 -0300

    Aprimoração e limpeza do código. Agora o jogo roda com menos operações!

--- adivinhacao.py
+++ adivinhacao.py
@@ -15,38 +15,28 @@ while jogar == 'Y':
   numero_secreto = random.randrange(1, 51)
   tentativas = 5
   rodada = 1
+  chute = 0
 
-  print(f'Rodada: {rodada} de {tentativas}')
-  chute_str = input('Escolha um número: ')
-  chute = int(chute_str)
-  maior = chute > numero_secreto
-  menor = chute < numero_secreto
+  while (chute != numero_secreto) & (rodada <= tentativas):
+    print(f'Rodada {rodada} de {tentativas}')
+    chute = int(input('Escolha um número: '))
 
-  while (chute != numero_secreto) & (rodada < tentativas):
-    rodada += 1
-    if maior:
+    if chute > numero_secreto:
       print('Você errou... O número secreto é menor!')
       print('')
-      print(f'Rodada: {rodada} de {tentativas}')
-
-    elif menor:
+    elif chute < numero_secreto:
       print('Você errou... O número secreto é maior!')
       print('')
-      print(f'Rodada: {rodada} de {tentativas}')
-    chute_str = input('Escolha um número: ')
-    chute = int(chute_str)
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+
+    rodada += 1
 
   if chute == numero_secreto:
-    print('')
     print(f'Parabéns, você acertou! O número secreto é {numero_secreto}!')
     print('')
   else:
     print('Errado de novo...')
-    print('')
     print('Você gastou todas as tentativas, sinto muito. Você é burrinho. Eu não faço as regras.')
-    print(f'(Caso você queira saber, o número secreto era {numero_secreto}')
+    print(f'(Caso você queira saber, o número secreto era {numero_secreto})')
     print('')
 
   print('*********************************')
