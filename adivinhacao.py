73f29c936ba97e3d9d4bf609f874f23d50a50c83
Luc <lucthemit@gmail.com>
2022-06-24 17:19:53 -0300

    Aprimoração do código: substituido o laço "while" por "for" para determinar o comportamento das rodadas; utilizando a declaração "break" para encerrar o jogo em caso de acerto, tornando a leitura do código mais fácil; aplicando a declaração "continue" para implementar a limitação de números inseridos para o range indicado.

--- adivinhacao.py
+++ adivinhacao.py
@@ -12,39 +12,42 @@ print('')
 jogar = 'Y'
 
 while jogar == 'Y':
-  numero_secreto = random.randrange(1, 51)
-  tentativas = 5
-  rodada = 1
-  chute = 0
+    numero_secreto = random.randrange(1, 51)
+    tentativas = 5
+    print(numero_secreto)
 
-  while (chute != numero_secreto) & (rodada <= tentativas):
-    print(f'Rodada {rodada} de {tentativas}')
-    chute = int(input('Escolha um número: '))
+    for rodada in range(1, tentativas + 1):
+        print(f'Rodada {rodada} de {tentativas}')
+        chute = int(input('Escolha um número entre 1 e 50: '))
 
-    if chute > numero_secreto:
-      print('Você errou... O número secreto é menor!')
-      print('')
-    elif chute < numero_secreto:
-      print('Você errou... O número secreto é maior!')
-      print('')
+        if (chute < 1) | (chute > 50):
+            print('Você deve digitar um número entre 1 e 100!')
+            print('')
+            continue
 
-    rodada += 1
+        if chute == numero_secreto:
+            print(f'Parabéns, você acertou! O número secreto é {numero_secreto}!')
+            print('')
+            break
+        if chute > numero_secreto:
+            print('Você errou... O número secreto é menor!')
+            print('')
+        elif chute < numero_secreto:
+            print('Você errou... O número secreto é maior!')
+            print('')
 
-  if chute == numero_secreto:
-    print(f'Parabéns, você acertou! O número secreto é {numero_secreto}!')
+    if chute != numero_secreto:
+        print('Errado de novo...')
+        print('Você gastou todas as tentativas, sinto muito. Você é burrinho. Eu não faço as regras.')
+        print(f'(Caso você queira saber, o número secreto era {numero_secreto})')
+        print('')
+
+    print('*********************************')
+    print('Obrigado por jogar!')
+    print('*********************************')
     print('')
-  else:
-    print('Errado de novo...')
-    print('Você gastou todas as tentativas, sinto muito. Você é burrinho. Eu não faço as regras.')
-    print(f'(Caso você queira saber, o número secreto era {numero_secreto})')
+    jogar = input('Você quer jogar de novo? Y para sim, qualquer tecla para encerrar. ').upper()
     print('')
-
-  print('*********************************')
-  print('Obrigado por jogar!')
-  print('*********************************')
-  print('')
-  jogar = input('Você quer jogar de novo? Y para sim, qualquer tecla para encerrar. ').upper()
-  print('')
 
 print('*********************************')
 print('Adeus!')
